a1b8b2a2e0d809221da7d5a086d23a359b622915
admin <efjulia@mail.ru>
2024-11-25 22:58:42 +0300

    main file

---
+++ main.py
@@ -0,0 +1,8 @@
+import io
+import sqlite3
+import sys
+from random import choice
+import datetime as dt
+from PyQt6 import uic
+from PyQt6.QtCore import QDate
+from PyQt6.QtWidgets import (QApplication, QMainWindow)
