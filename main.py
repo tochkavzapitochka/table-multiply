bb9d1860e1463cf2f61ab89e1efbc974dc763816
tochkavzapitochka <efjulia@mail.ru>
2025-03-06 16:18:16 +0300

    проба2

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ import datetime as dt
 from PyQt6.QtCore import QDate
 from PyQt6.QtWidgets import (QApplication, QMainWindow)
 import io
-import random
 from PyQt6 import uic  # Импортируем uic
 
 template1 = """<?xml version="1.0" encoding="UTF-8"?>
